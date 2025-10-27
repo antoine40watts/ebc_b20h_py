bc4e0eee9f3fd450b543af499625a8870c654e16
Gweltaz DG <gweltou@hotmail.com>
2025-10-27 16:30:16 +0100

    Run servers as user 'watts40' for permissions

--- run.sh
+++ run.sh
@@ -30,10 +30,10 @@ else
     echo "Github.com is not accessible" >> github_error.log
 fi
 
-uvicorn main:app --reload --host 0.0.0.0 &
+sudo -u watts40 uvicorn main:app --reload --host 0.0.0.0 &
 
 # Start the Svelte development server (or build your Svelte app)
 cd front
-npm run dev -- --host 0.0.0.0 --port 80  # Start the development server
+sudo -u watts40 npm run dev -- --host 0.0.0.0 --port 80  # Start the development server
 
 # Use 'npm run build' for production deployment instead of 'npm run dev'
