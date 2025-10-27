5e4d1abf44f22989ec6120db306993b310deb86a
Gweltaz DG <gweltou@hotmail.com>
2025-10-27 16:56:38 +0100

    Change Svelte dev server port from 80 to 3000

--- run.sh
+++ run.sh
@@ -34,6 +34,6 @@ uvicorn main:app --reload --host 0.0.0.0 &
 
 # Start the Svelte development server (or build your Svelte app)
 cd front
-npm run dev -- --host 0.0.0.0 --port 80  # Start the development server
+npm run dev -- --host 0.0.0.0 --port 3000  # Start the development server
 
 # Use 'npm run build' for production deployment instead of 'npm run dev'
